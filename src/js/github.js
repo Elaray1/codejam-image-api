d7c1dbc5d9cdbb5bc4c0085f923eae9ebe7f5aff
Egor <5642452@mail.ru>
2019-11-16 19:45:23 +0300

    11

--- src/js/github.js
+++ src/js/github.js
@@ -6,7 +6,8 @@ anchorTag.addEventListener('click', (e) => {
   const authenticator = new netlify.default ({})
   authenticator.authenticate({provider:"github", scope: "user"}, (err, data) => {
     err ? outputText.innerText = "Error Authenticating with GitHub: " + err :
-    outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token
+    outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token;
+    console.log(data);
   })
 })
 // }
